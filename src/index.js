470481868bc74ed81691d3cdf7f6295e7a82068c
Yifan <datasciencewyfan@gmail.com>
2022-05-26 08:53:19 -0700

    Challenge 1 and 2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
-console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    startFetchImg()
+    startFetchBreed()
+})
+
+function startFetchImg() {
+    fetch(imgUrl)
+        .then(resp => resp.json())
+        .then(objects =>{
+            renderDogs(objects.message)
+        })
+}
+
+function renderDogs(urls) {
+    urls.forEach(url =>{
+        const dogImageContainer = document.getElementById('dog-image-container')
+        
+        let img = document.createElement('img')
+        img.setAttribute('src' , url)
+        dogImageContainer.append(img)
+    })
+}
+
+function startFetchBreed() {
+    fetch(breedUrl)
+        .then(resp => resp.json())
+        .then(objects => {
+            renderBreeds(Object.keys(objects.message))
+        })
+}
+
+function renderBreeds(breeds) {
+    breeds.forEach(breed => {
+        const dogBreeds = document.getElementById('dog-breeds')
+
+        let li = document.createElement('li')
+        li.textContent = breed
+        dogBreeds.append(li)
+    })
+}
